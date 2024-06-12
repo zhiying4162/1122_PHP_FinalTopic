432cb27baa52e86bd4f34acbbde6f4506fec6f74
Zhiying <melody9306@gmail.com>
2024-06-12 10:18:44 +0800

    insert Leave

---
+++ Leave.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css">
+
+<title>員工請假</title>
+
+</head>
+
+<body>
+<div class="container">
+<form id="form1" name="form1" method="post" action="checkin_insert.php" onsubmit="return handleSubmit(event)">
+	<?php
+	
+		$options = [
+        "option1" => "事假",
+        "option2" => "病假"
+		];
+	?>
+
+  <h1>請假</h1>
+
+  <p>
+  <input type="button" name="leave" id="leave" value="請假" onclick="leaveContent()" />
+    <div id="hidden-content">
+        <h2>請假</h2>
+		 <p>
+			<label for="leaveList">假別：</label>
+			<select name="leaveList" id="leaveList" required="required">
+				<?php
+				foreach ($options as $value => $label) {
+					echo '<option value="' . $value . '">' . $label . '</option>';
+				}
+			?>
+			</select>
+		</p>
+		<p>
+			<label for="starttime">申請起始日期：</label>
+			<input name="starttime" type="date" value="<?php echo date('Y-m-d'); ?>" required="required" />
+		</p>
+		<p>
+			<label for="finishtime">申請結束日期：</label>
+			<input name="finishtime" type="date" value="<?php echo date('Y-m-d'); ?>" required="required" />
+		</p>
+		<p>
+			<textarea name="mytext" rows="6" cols="40" required="required" placeholder="請假原因"></textarea>
+		</p>
+		<p>
+		   <input type="submit" value="送出" />
+    </div>
+  <input type="button" onclick="history.go(-1)" value="回到選單頁" />
+</form>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ Leave_insert.php
@@ -0,0 +1,44 @@
+<?php
+date_default_timezone_set('Asia/Shanghai'); // 設定時區
+
+session_start();
+$id = $_SESSION['id'];
+$Name = $_SESSION['Name'];
+
+    
+    
+require_once 'DB_conn.php';
+        
+    $leaveList = $_POST['leaveList'];
+    $starttime = $_POST['starttime'];
+    $finishtime = $_POST['finishtime'];
+    $mytext = $_POST['mytext'];
+
+    $sql = "INSERT INTO afleave VALUES ('$id','$leaveList','$starttime','$finishtime','$mytext')";
+    $result = mysqli_query($link,$sql);
+        
+    if($result){
+        echo '<form method="post" action="checkin_insert.php">';
+        echo "<h1>請假成功</h1>";
+        echo "<p>員工姓名：$CName</p>";
+        echo "<p>員工編號：$ID</p>";
+        echo "<p>假別：$leaveList</p>";
+        echo "<p>請假時間：$starttime. 到 .$finishtime</p>";
+        echo "<p>請假原因：$mytext</p>";
+        echo '<input type="submit" name="MainMenu" id="MainMenu" value="返回主選單" />';
+        echo '</form>';
+    }
+
+    mysqli_close($link); 
+ 
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['MainMenu'])) {
+            header("Location: Employeecheck.php");
+            exit();
+        } 
+    }
+
+    
+    
+    ?>
